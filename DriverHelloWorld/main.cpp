8bdaffeb24227941d67bdbe7c546e2126f6bd132
kiennguyen-246 <duckienntt1@gmail.com>
2024-08-19 18:05:54 +0700

    19/08/22 Afterwork Commit (2)

---
+++ DriverHelloWorld/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ntddk.h>
+#include <wdf.h>
+
+/**
+ *  The entry point function for the device,
+ *  @param pDriverObject Pointer to the DRIVER_OBJECT (DO) representing this driver
+ *  @param pRegistryPath A string pointing to the location in the registry where information for the driver was stored
+ */
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
+{
+    NTSTATUS ntStatus = STATUS_SUCCESS;
+    
+}
